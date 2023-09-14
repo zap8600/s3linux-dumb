81820750d0792e82bc1c4094e383125002c15af5
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-14 13:28:09 -0500

    fix for 2048

--- build-2048.sh
+++ build-2048.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 wget https://raw.githubusercontent.com/mevdschee/2048.c/master/2048.c
-xtensa-linux-uclibcfdpic-gcc -o 2048 2048.c -mfdpic
+xtensa-esp32s3-linux-uclibcfdpic-gcc -o 2048 2048.c -mfdpic
