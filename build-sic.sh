c6aa7d4da720c5b6053fd5b42dc59840df2ee1b9
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-09-14 13:27:36 -0500

    fix for sic

--- build-sic.sh
+++ build-sic.sh
@@ -2,5 +2,6 @@
 
 git clone http://git.suckless.org/sic
 pushd sic
-make CC=xtensa-linux-uclibcfdpic-gcc CFLAGS+=-mfdpic
+make CC=xtensa-esp32s3-linux-uclibcfdpic-gcc LDFLAGS="-mfdpic"
+cp sic ..
 popd
